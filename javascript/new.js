2a1f664e62942aa8320341400758b7cd45b92d1f
rameez14 <rameez_hussain@live.co.uk>
2017-12-11 16:25:28 +0000

    css animation for H.RAMEEZ

--- javascript/new.js
+++ javascript/new.js
@@ -1,19 +1,3 @@
-setInterval(displayclock, 500);
-function displayclock(){
-	var time = new Date();
-	var hrs = time.getHours();
-	var min = time.getMinutes();
-	var sec = time.getSeconds();
-	
-	
-	
-	if (hrs ==0) {
-		hrs = 12;
-	}
-	
-	document.getElementById('clock').innerHTML = hrs + ':' + min + ':' + sec;
-	
-}
 
 function checkforblank(){
 	if(document.getElementById('fname').value=="") {
