2c7834eac0ea7ab63de0ed18ae74cf0c93aa5dda
rameez14 <rameez_hussain@live.co.uk>
2017-12-21 17:20:31 +0000

    Javascript quiz about myself

--- javascript/new.js
+++ javascript/new.js
@@ -38,10 +38,10 @@ function initMap() {
 }
 setInterval(dispalyclock, 500);
 function dispalyclock(){
-	var time = new Date();
-	var hrs = time.getHours();
-	var min = time.getMinutes();
-	var sec = time.getSeconds();
+	let time = new Date();
+	let hrs = time.getHours();
+	let min = time.getMinutes();
+	let sec = time.getSeconds();
 	
 
 	
@@ -55,5 +55,50 @@ function dispalyclock(){
 	
 	
 	}
+
+	let pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+let questions = [
+    [ "What town was I born in?", "Leicester", "Bedford", "London", "B" ],
+	[ "What university am I currently studying in?", "University of Leicester", "Brunel university", "De Montfort university", "C" ],
+	[ "What degree am I currently studying?", "Computer Science", "Computer Security", "Software Engineering", "A" ],
+	[ "What secondary school did I attend?", "Kingsbury High School ", "Beaumont Leys School", "Biddenham Upper School", "C" ]
+];
+function _(x){
+	return document.getElementById(x);
+}
+function renderQuestion(){
+	test = _("test");
+	if(pos >= questions.length){
+		test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions right</h2>";
+		_("test_status").innerHTML = "You have now compleated your test";
+		pos = 0;
+		correct = 0;
+		return false;
+	}
+	_("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+	question = questions[pos][0];
+	chA = questions[pos][1];
+	chB = questions[pos][2];
+	chC = questions[pos][3];
+	test.innerHTML = "<h3>"+question+"</h3>";
+	test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
+	test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
+	test.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br><br>";
+	test.innerHTML += "<button onclick='checkAnswer()'>Submit</button>";
+}
+function checkAnswer(){
+	choices = document.getElementsByName("choices");
+	for(let i=0; i<choices.length; i++){
+		if(choices[i].checked){
+			choice = choices[i].value;
+		}
+	}
+	if(choice == questions[pos][4]){
+		correct++;
+	}
+	pos++;
+	renderQuestion();
+}
+window.addEventListener("load", renderQuestion, false);
 
 
